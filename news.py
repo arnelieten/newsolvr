252e325ca2b533ffed9654ee180cd0ccf040104e
Arne Lieten <arne.lieten@hotmail.com>
2025-05-17 22:27:43 +0200

    Some small tweaks nothing majorly important

--- news.py
+++ news.py
@@ -2,7 +2,8 @@ from api_utils import api_call
 import pandas as pd
 from db_utils import connect_to_db, run_query, close_query
 
-news_data = api_call("Marathon")
+topic = "problem"
+news_data = api_call(topic)
 
 df = pd.json_normalize(news_data, 'articles')
 df = df[['title', 'description', 'content', 'publishedAt', 'url']]
