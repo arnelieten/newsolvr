eb7c8f6719689e011af7eeaa43ebb284c3a3dd1c
Arne Lieten <arne.lieten@hotmail.com>
2025-05-18 12:53:14 +0200

    refactored code to 2 functions

--- news.py
+++ news.py
@@ -2,21 +2,27 @@ from api_utils import api_call
 import pandas as pd
 from db_utils import connect_to_db, run_query, close_query
 
-topic = "problem"
-news_data = api_call(topic)
+def transform_news_api(topic):
+    news_data = api_call(topic)
 
-df = pd.json_normalize(news_data, 'articles')
-df = df[['title', 'description', 'content', 'publishedAt', 'url']]
-df = df.dropna(subset=['title', 'description', 'content'])
-df = df.drop_duplicates(subset='title')
+    df = pd.json_normalize(news_data, 'articles')
+    df = df[['title', 'description', 'content', 'publishedAt', 'url']]
+    df = df.dropna(subset=['title', 'description', 'content'])
+    df = df.drop_duplicates(subset='title')
+    return df
 
-dcb = connect_to_db()
-for row in df.itertuples(index=False):
-    run_query(dcb, 
-        """INSERT INTO public.problems 
-           (title_article, description_article, content_article, link_article, published_date) 
-           VALUES (%s, %s, %s, %s, %s)
-           ON CONFLICT (link_article) DO NOTHING""", 
-        (row.title, row.description, row.content, row.url, row.publishedAt))
+def insert_news_data(df):
+    dcb = connect_to_db()
+    for row in df.itertuples(index=False):
+        run_query(dcb, 
+            """INSERT INTO public.problems 
+            (title_article, description_article, content_article, link_article, published_date) 
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (link_article) DO NOTHING""", 
+            (row.title, row.description, row.content, row.url, row.publishedAt))
+    close_query(dcb)
 
-close_query(dcb)
+
+current_topic = "problem"
+df = transform_news_api(current_topic)
+insert_news_data(df)
