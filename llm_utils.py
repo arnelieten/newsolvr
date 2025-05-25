ae3dcd3e8135c5a3e15681c912d326bdc3119efa
Arne Lieten <arne.lieten@hotmail.com>
2025-05-25 14:26:59 +0200

    simple llm connection made. Next steps: fetch database articles and input them in to llm_call function

--- llm_utils.py
+++ llm_utils.py
@@ -1,13 +1,14 @@
 import json
 from google import genai
 
-with open("llm_api_credentials.json", "r") as f:
-        headers = json.load(f)
-        API_KEY = headers['api-key']
-    
-client = genai.Client(api_key=API_KEY)
+def llm_call(input):
+    with open("llm_api_credentials.json", "r") as f:
+            headers = json.load(f)
+            API_KEY = headers['api-key']
+        
+    client = genai.Client(api_key=API_KEY)
 
-response = client.models.generate_content(
-    model="Gemini 2.5 Flash Preview 05-20", contents="Explain how AI works in a few words"
-)
-print(response.text)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=input
+    )
+    print(response.text)
