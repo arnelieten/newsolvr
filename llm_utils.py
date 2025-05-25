0a020eb811102a74351228ee60a3ddf822a72428
Arne Lieten <arne.lieten@hotmail.com>
2025-05-25 21:35:02 +0200

    modified llm_utils to provide the output in json format and answering the right questions

--- llm_utils.py
+++ llm_utils.py
@@ -1,5 +1,7 @@
 import json
 from google import genai
+from google.genai import types
+from pydantic import BaseModel
 from db_utils import connect_to_db, close_query, get_query
 
 def fetch_news_article():
@@ -8,19 +10,32 @@ def fetch_news_article():
      close_query(dcb)
      return articles
 
+class Analysis(BaseModel):
+    problem_verified: str
+    problem_summary: str
+    target_market: str
+    startup_idea: str
+    business_model: str
+
 def llm_call():
     with open("llm_api_credentials.json", "r") as f:
             headers = json.load(f)
             API_KEY = headers['api-key']
     
     articles = fetch_news_article()
-    article = articles[1]
+    article = articles[9]
 
     client = genai.Client(api_key=API_KEY)
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash", contents=article
-    )
-    print(response.text)
+        model="gemini-2.0-flash",
+        config=types.GenerateContentConfig(
+            system_instruction = "You are experienced serial entrepreneur that is doing market research, for this you scan the internet for relevant problems. The input you are getting is a newsarticle and I am asking you for your perspective. Question 1: is this really a problem where someone could build a startup around, be strict and put yes or no in 'problem_verified' of the response schema. Question 2: if it is a real problem summarize the problem, if not a real problem just put N/A 'problem_verified' of the response schema. Question 3: How big is the target market for this problem in euros, if you do not know put 0 in 'target_market' of the response schema. Question 4: Be creative and invent a startup idea and put a short description in 'startup_idea' of the response schema. Question 5: based on your invented startup idea, is it B2C or B2B and put that answer in 'business_model' of the response schema.",
+            max_output_tokens = 1000,
+            response_mime_type = "application/json",
+            response_schema= list[Analysis]),
+        contents=article)
+    report = response.text
+    return report
 
 llm_call()
