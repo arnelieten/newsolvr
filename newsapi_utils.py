db2989b0266b09b7ff39e5bd775e0ec3c0adaf17
Arne Lieten <arne.lieten@hotmail.com>
2025-05-25 08:01:12 +0200

    restructuring codebase a bit to make more clear, put more functions in newsapi_utils.py

--- newsapi_utils.py
+++ newsapi_utils.py
@@ -1,7 +1,9 @@
 import requests
 import json
+import pandas as pd
+from db_utils import connect_to_db, run_query, close_query
 
-def api_call(topic):
+def api_call(topic, from_date, to_date):
     base_url = "https://newsapi.org/v2/everything"
 
     with open("api_credentials.json", "r") as f:
@@ -10,8 +12,8 @@ def api_call(topic):
     params = {
         "q" : topic,
         "language" : "en",
-        "from" : "2025-05-02",
-        "to" : "2025-05-03"
+        "from" : from_date,
+        "to" : to_date
 
     }
 
@@ -22,3 +24,24 @@ def api_call(topic):
         return news_data
     else:
         print(f"Failed to connect with API {response.status_code}")
+
+
+def transform_news_api(topic, from_date, to_date):
+    news_data = api_call(topic, from_date, to_date)
+
+    df = pd.json_normalize(news_data, 'articles')
+    df = df[['title', 'description', 'content', 'publishedAt', 'url']]
+    df = df.dropna(subset=['title', 'description', 'content'])
+    df = df.drop_duplicates(subset='title')
+    return df
+
+def insert_news_api(df):
+    dcb = connect_to_db()
+    for row in df.itertuples(index=False):
+        run_query(dcb, 
+            """INSERT INTO public.newsolvr 
+            (title_article, description_article, content_article, link_article, published_date) 
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (link_article) DO NOTHING""", 
+            (row.title, row.description, row.content, row.url, row.publishedAt))
+    close_query(dcb)
--- newsolvr.py
+++ newsolvr.py
@@ -1,28 +1,10 @@
-from newsapi_utils import api_call
+from newsapi_utils import insert_news_api, transform_news_api
 import pandas as pd
-from db_utils import connect_to_db, run_query, close_query
+from datetime import datetime, timedelta
 
-def transform_news_api(topic):
-    news_data = api_call(topic)
+current_topic = "problem NOT no problem"
+from_date = datetime.now() - timedelta(minutes=1620)
+to_date = datetime.now() - timedelta(minutes=1560)
 
-    df = pd.json_normalize(news_data, 'articles')
-    df = df[['title', 'description', 'content', 'publishedAt', 'url']]
-    df = df.dropna(subset=['title', 'description', 'content'])
-    df = df.drop_duplicates(subset='title')
-    return df
-
-def insert_news_api(df):
-    dcb = connect_to_db()
-    for row in df.itertuples(index=False):
-        run_query(dcb, 
-            """INSERT INTO public.newsolvr 
-            (title_article, description_article, content_article, link_article, published_date) 
-            VALUES (%s, %s, %s, %s, %s)
-            ON CONFLICT (link_article) DO NOTHING""", 
-            (row.title, row.description, row.content, row.url, row.publishedAt))
-    close_query(dcb)
-
-
-current_topic = "problem"
-df = transform_news_api(current_topic)
+df = transform_news_api(current_topic, from_date, to_date)
 insert_news_api(df)
